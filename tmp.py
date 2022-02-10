bc00598358c1b6cd9f8cff7233ca32219d7cf230
OUCzhangzheng <1206064593@qq.com>
2022-02-10 22:21:29 +0800

    Test

--- tmp.py
+++ tmp.py
@@ -3,20 +3,5 @@
 # @Email    : zz19970227@gmail.com
 # @File     : tmp.py
 # @Project  : QuantitativePrecipitationEstimation
-import os
-import pyart
 
-path = "/media/data3/zhangjianchang/RadarData/2020"
-files = os.listdir(path)
-
-for number in range(len(files)):
-    radar = pyart.io.read_nexrad_archive(os.path.join(path, files[number]))
-    print(radar.time["data"][-1])
-    # print(radar.metadata)
-    print(radar.fixed_angle)
-    print(radar.fixed_angle["data"].shape)
-    print(radar.metadata["vcp_pattern"])
-    if number == 50:
-        break
-    print("-"*20)
-
+print("Hello world!")
